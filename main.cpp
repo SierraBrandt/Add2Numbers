0c1f047348e2c23b1550b2b50369e0f836257aa5
Sierra Brandt <44011600+SierraBrandt@users.noreply.github.com>
2021-09-07 19:23:23 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+// Add2Numbers.cpp - Add two whole numbers together and print the results
+// Author: Sierra Brandt
+#include <iostream>
+using namespace std;
+int main(){    
+  // Create three int variables to add using random values    
+  int a = 52;    
+  int b = 39;  
+  int c = 25;  
+  // Calculate their sum and print the result    
+  int sum = a + b +c;    
+  cout << "The sum of " << a << ", " << b << " and "<< c <<" is " << sum << endl;    return 0 ;
+}
